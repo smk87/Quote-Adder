75287ec693f4cb9b5054d4c585086a4eab0ae3c8
Saqlain Mahmud <30999133+smk87@users.noreply.github.com>
2019-09-02 09:50:56 +0600

    Quotes displayed.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
 </template>
 
 <script>
+import QuoteGridVue from "./components/QuoteGrid.vue";
+
 export default {
   data() {
     return {
       quotes: ["Just a quote to see something."],
       maxQuotes: 10
     };
+  },
+  components: {
+    appQuoteGrid: QuoteGridVue
   }
 };
 </script>
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="row"></div>
+  <div class="row">
+    <app-quote v-for="quote in quotes">{{quote}}</app-quote>
+  </div>
 </template>
 
 <script>
+import QuoteVue from "./Quote.vue";
+
 export default {
-  props: ["quotes"]
+  props: ["quotes"],
+  components: {
+    appQuote: QuoteVue
+  }
 };
 </script>
 
